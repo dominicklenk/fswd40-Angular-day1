1bb28e95b84180fb6331c08bf1d6550017f2e8a8
dominicklenk <dominic_klenk@yahoo.at>
2018-05-24 01:14:39 +0200

    add prework exercise

---
+++ prework_exercise/homepage/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+{
+  path: '', component: HomePageComponent
+},
+{
+  path: 'about', component: AboutPageComponent
+},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomePageComponent,
+    AboutPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
